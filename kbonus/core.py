827a3e1b9575c82f172339b776220213548bcded
zclaytor <zclaytor@hawaii.edu>
2023-11-09 10:42:42 -0500

    add catalog reader and test

--- kbonus/core.py
+++ kbonus/core.py
@@ -1,3 +1,22 @@
 import os
+import pandas as pd
+from astropy.table import Table
+from astropy.io import fits
+import lightkurve as lk
+
 
 root_dir = "/home/zach/Projects/kbonus-bkg/"
+
+
+def read_input_catalog(reader="fits", **kw):
+    if reader == "fits":
+        return Table.read(
+            os.path.join(root_dir, 
+                "hlsp_kbonus-bkg_kepler_kepler_source-catalog_kepler_v1.0_cat.fits"),
+            format="fits",
+            **kw)
+    elif reader == "pandas":
+        return pd.read_csv(
+            os.path.join(root_dir, "kbonus-bkg_source_catalog_v1.0.fits"),
+            **kw
+        )
--- tests/test_kbonus.py
+++ tests/test_kbonus.py
@@ -1,5 +1,14 @@
 import os
+import pandas as pd
+from astropy.table import Table
+
 import kbonus as kb
+
 
 def test_root_exists():
     assert os.path.exists(kb.root_dir)
+
+def test_reader():
+    assert isinstance(kb.read_input_catalog(), Table)
+    assert isinstance(kb.read_input_catalog(reader="pandas"), pd.DataFrame)
+    
