02be83416368b3becbba1c8d33e133e539670699
zclaytor <zclaytor@hawaii.edu>
2023-11-09 13:22:38 -0500

    add end to end test

--- tests/test_kbonus.py
+++ tests/test_kbonus.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from astropy.table import Table
+import lightkurve as lk
 
 import kbonus as kb
 
@@ -15,4 +16,19 @@ def test_reader():
         kb.read_input_catalog('pasta')
     except ValueError:
         pass
+
+def test_resolve_filename():
+    gaia = 2143858906058582784
+    kic = 11282447
+    fname = 'kic-011282447'
+    f1 = kb.core.resolve_filename(gaia)
+    f2 = kb.core.resolve_filename(kic)
+    assert f1 == fname
+    assert f2 == fname
     
+def test_ETE():
+    gaia = 2086636884980516352
+    kic = 10407233
+    for f in [gaia, kic, f"Gaiadr3{gaia}  ", f"   kic{kic}"]:
+        l = kb.get_lightcurve(f)
+        assert isinstance(l, lk.LightCurve)
