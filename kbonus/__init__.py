fc64dc7df8dd220f701c4773d2a678e91c2fd18b
zclaytor <zclaytor@hawaii.edu>
2023-11-11 10:56:23 -0500

    move version source to __init__.py

--- kbonus/__init__.py
+++ kbonus/__init__.py
@@ -4,3 +4,5 @@ from .version import __version__
 from .core import root_dir
 from .core import read_catalog, read_source_catalog, read_mstars_catalog, read_wd_catalog
 from .core import get_lightcurve, get_quarter_lightcurves
+
+__version__ = "1.0.0-b"
--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
+import codecs
+import os.path
 
-# Load the __version__ variable without importing the package already
-exec(open("kbonus/version.py").read())
+
+# These two functions are just to read the version number. Sourced from
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setuptools.setup(
     name="kbonus",
-    version=__version__,
+    version=get_version("package/__init__.py"),
     author="Zachary R. Claytor",
     author_email="zclaytor@ufl.edu",
     description="Utilities for reading kbonus-bkg light curves from HiPerGator",
